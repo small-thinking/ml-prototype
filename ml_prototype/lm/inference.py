dbe5c313e3d8b9f27808b1250d8402c0bf434a51
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-09-25 22:26:05 -0700

    Refactor

--- ml_prototype/lm/inference.py
+++ ml_prototype/lm/inference.py
@@ -1,69 +1,61 @@
 import os
 
 import torch
+from tokenizer import NaiveTokenizer, Tokenizer
 from torch.nn.functional import softmax
 
-from ml_prototype.lm.module import LanguageModule, TransformerLM
-from ml_prototype.lm.tokenizer import NaiveTokenizer, Tokenizer
+device = "cpu"
+device = "cuda:0"
 
 
 class InferenceEngine:
-    def __init__(self, tokenizer: Tokenizer, jit_model_path: str):
-        # Load the model checkpoint
-        self.model = torch.jit.load(jit_model_path)
-        self.model.eval()  # Set the model to evaluation mode
+    def __init__(self, tokenizer, jit_model_path: str):
+        self.model = torch.jit.load(jit_model_path).to(device)  # Move model to CPU
+        self.model.eval()
         self.tokenizer = tokenizer
 
-    def inference(self, text: str, max_length: int = 50):
+    def inference(self, text: str, max_length: int = 128, temperature: float = 1.0):
         print(f"Inference: {text}")
-        # Tokenize the input text
-        input_tensor = self.tokenizer.encode([text])
-        # Initialize generated sequence with the input tensor
+        input_tensor = self.tokenizer.encode([text]).to(device)  # Move tensor to CPU
         generated_sequence = input_tensor
 
-        # Generate predictions in an autoregressive way
         with torch.no_grad():
-            for _ in range(2):
-                logits = self.model(generated_sequence)
-                # Take the logits corresponding to the last token
+            for _ in range(max_length):
+                generated_sequence_crop = generated_sequence[:, -256:]
+                logits = self.model(generated_sequence_crop)
+                # Get the logit of the last token
                 last_logits = logits[:, -1, :]
-                # Apply a softmax to get probabilities
-                probabilities = softmax(last_logits, dim=-1)
-                # Get the most likely token
-                res1 = torch.argmax(probabilities, dim=-1)
-                next_token = torch.argmax(probabilities, dim=-1).unsqueeze(1)
+                # Adjust logits with temperature
+                adj_logits = last_logits / temperature
+                # Apply softmax to get probabilities
+                adj_prob = softmax(adj_logits, dim=-1)
 
-                # Check whether the generated sequence is less than context_size
-                if generated_sequence.size(1) < max_length:
-                    # Append the next token without removing any tokens
-                    generated_sequence = torch.cat(
-                        [generated_sequence, next_token], dim=1
-                    )
-                else:
-                    # Remove the first token and append the next token
-                    generated_sequence = generated_sequence[:, 1:]
-                    generated_sequence = torch.cat(
-                        [generated_sequence, next_token], dim=1
-                    )
+                next_token = torch.multinomial(adj_prob, num_samples=1).to(
+                    device
+                )  # Move tensor to CPU
+                generated_sequence = torch.cat([generated_sequence, next_token], dim=1)
 
-        # Decode the generated sequence to text
-        decoded_text = self.tokenizer.decode(generated_sequence[0])
+                # Decode the next token to text and yield it
+                next_token_text = self.tokenizer.decode(next_token[0])
+                yield next_token_text
 
-        return decoded_text
+                if generated_sequence.size(1) >= max_length:
+                    break
 
 
 def main():
-    doc_file_path = os.path.expanduser("~/Downloads/test_data.txt")
-    tokenizer = NaiveTokenizer(config={}, doc_file_path=doc_file_path)
+    token_file = os.path.expanduser("./data/tokens.json")
+    tokenizer = NaiveTokenizer(config={}, token_file=token_file)
     inference_engine = InferenceEngine(
         tokenizer=tokenizer,
-        jit_model_path=os.path.join(os.path.dirname(__file__), "../../model.pt"),
+        jit_model_path=os.path.join(
+            os.path.dirname(__file__), "../../model_epoch_20.pt"
+        ),
     )
 
-    text = "The quick brown fox"
-    decoded_text = inference_engine.inference(text[:16])
-
-    print("Predicted Text:", decoded_text)
+    text = "To be or not to be. This is a question. What is the matter of the universe?"
+    for next_token_text in inference_engine.inference(text[:256]):
+        print(next_token_text, end="")
 
 
 if __name__ == "__main__":
