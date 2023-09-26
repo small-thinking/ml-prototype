b65a7a5f60dcca4584e3067c594fae03c0592537
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-09-25 22:26:05 -0700

    Refactor

--- ml_prototype/lm/module.py
+++ ml_prototype/lm/module.py
@@ -2,10 +2,11 @@
 """
 import abc
 import math
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import torch
 import torch.nn as nn
+from torch.nn.functional import pad
 
 
 class LanguageModule(nn.Module, abc.ABC):
@@ -35,12 +36,12 @@ class RMSNorm(nn.Module):
         self.config = config
         self.layer_size = layer_size
         self.eps = eps
-        self.has_bias = has_bias
-        self.scale = nn.Parameter(torch.ones(layer_size))
-        self.register_parameter("scale", self.scale)
-        if self.has_bias:
-            self.bias = nn.Parameter(torch.zeros(layer_size))
-            self.register_parameter("bias", self.bias)
+        # self.has_bias = has_bias
+        # self.scale = nn.Parameter(torch.ones(layer_size))
+        # self.register_parameter("scale", self.scale)
+        # if self.has_bias:
+        #     self.bias = nn.Parameter(torch.zeros(layer_size))
+        #     self.register_parameter("bias", self.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -57,117 +58,117 @@ class RMSNorm(nn.Module):
             return self.scale * x_normed
 
 
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self, temperature: int = 1.0, dropout_ratio: int = 0.0):
-        super().__init__()
-        self.temperature = temperature
-        self.dropout = nn.Dropout(dropout_ratio)
+# class ScaledDotProductAttention(nn.Module):
+#     def __init__(self, temperature: int = 1.0, dropout_ratio: int = 0.0):
+#         super().__init__()
+#         self.temperature = temperature
+#         self.dropout = nn.Dropout(dropout_ratio)
 
-    def forward(self, q, k, v) -> torch.Tensor:
-        """Forward pass for Scaled Dot Product Attention.
-        An implementation of softmax(QK^T/sqrt(d_k)) * V.
+#     def forward(self, q, k, v) -> torch.Tensor:
+#         """Forward pass for Scaled Dot Product Attention.
+#         An implementation of softmax(QK^T/sqrt(d_k)) * V.
 
-        Args:
-            q (torch.Tensor): The query tensor, in shape [n, q_len, d_k].
-            k (torch.Tensor): The key tensor, in shape [n, k_len, d_k].
-            v (torch.Tensor): The value tensor, in shape [n, k_len, d_v].
+#         Args:
+#             q (torch.Tensor): The query tensor, in shape [n, q_len, d_k].
+#             k (torch.Tensor): The key tensor, in shape [n, k_len, d_k].
+#             v (torch.Tensor): The value tensor, in shape [n, k_len, d_v].
 
-        Returns:
-            torch.Tensor: The context vector after attention, in shape [n, k_len, d_v].
-        """
-        dk = q.shape[-1]
-        attn = (q @ k) / math.sqrt(dk)
-        attn = F.softmax(attn / self.temperature, dim=-1)
-        output = torch.matmul(self.dropout(attn), v)
-        return output
+#         Returns:
+#             torch.Tensor: The context vector after attention, in shape [n, k_len, d_v].
+#         """
+#         dk = q.shape[-1]
+#         attn = (q @ k) / math.sqrt(dk)
+#         attn = F.softmax(attn / self.temperature, dim=-1)
+#         output = torch.matmul(self.dropout(attn), v)
+#         return output
 
 
-class Attention(nn.Module):
-    def __init__(
-        self,
-        embed_size: int,
-        hidden_size: int,
-        num_heads: int,
-        temperature: int = 1.0,
-        dropout_ratio: int = 0.0,
-    ):
-        """Constructor of the Attention class."""
-        super(Attention, self).__init__()
+# class Attention(nn.Module):
+#     def __init__(
+#         self,
+#         embed_size: int,
+#         hidden_size: int,
+#         num_heads: int,
+#         temperature: int = 1.0,
+#         dropout_ratio: int = 0.0,
+#     ):
+#         """Constructor of the Attention class."""
+#         super(Attention, self).__init__()
 
-        self.embed_size = embed_size
-        self.hidden_size = hidden_size
-        self.num_heads = num_heads
+#         self.embed_size = embed_size
+#         self.hidden_size = hidden_size
+#         self.num_heads = num_heads
 
-        # Split embedding size into heads, validated by `embed_size % num_heads == 0`.
-        assert (
-            embed_size % num_heads == 0
-        ), f"embed_size ({embed_size}) must be divisible by num_heads ({num_heads})."
-        self.head_size = embed_size // num_heads
+#         # Split embedding size into heads, validated by `embed_size % num_heads == 0`.
+#         assert (
+#             embed_size % num_heads == 0
+#         ), f"embed_size ({embed_size}) must be divisible by num_heads ({num_heads})."
+#         self.head_size = embed_size // num_heads
 
-        # Initialize linear layers for projection and query.
-        self.q = nn.Linear(self.hidden_size, self.embed_size)
-        self.k = nn.Linear(self.hidden_size, self.embed_size)
-        self.v = nn.Linear(self.hidden_size, self.embed_size)
+#         # Initialize linear layers for projection and query.
+#         self.q = nn.Linear(self.hidden_size, self.embed_size)
+#         self.k = nn.Linear(self.hidden_size, self.embed_size)
+#         self.v = nn.Linear(self.hidden_size, self.embed_size)
 
-        # Scaled dot product layer.
-        self.scaled_dot_product_attention = ScaledDotProductAttention(
-            temperature=temperature, dropout_ratio=dropout_ratio
-        )
+#         # Scaled dot product layer.
+#         self.scaled_dot_product_attention = ScaledDotProductAttention(
+#             temperature=temperature, dropout_ratio=dropout_ratio
+#         )
 
-        # Output projection layer.
-        self.output = nn.Linear(self.embed_size, self.hidden_size)
+#         # Output projection layer.
+#         self.output = nn.Linear(self.embed_size, self.hidden_size)
 
-        # Allow registering additional attention layers.
-        self.attention_layers = nn.ModuleDict()
+#         # Allow registering additional attention layers.
+#         self.attention_layers = nn.ModuleDict()
 
-    def forward(
-        self,
-        q: torch.Tensor,
-        k: torch.Tensor,
-        v: torch.Tensor,
-        mask: torch.Tensor = None,
-    ):
-        """
-        Forward pass for the Attention module.
+#     def forward(
+#         self,
+#         q: torch.Tensor,
+#         k: torch.Tensor,
+#         v: torch.Tensor,
+#         mask: torch.Tensor = None,
+#     ):
+#         """
+#         Forward pass for the Attention module.
 
-        Parameters:
-        q (torch.Tensor): The query for attention, with shape (batch_size, hidden_size).
-        k (torch.Tensor): The keys for attention, with shape (batch_size, seq_len, hidden_size).
-        v (torch.Tensor): The values for attention, with shape (batch_size, seq_len, hidden_size).
-        mask (torch.Tensor, optional): The attention mask, with shape (batch_size, seq_len). 1 indicates valid token, 0 indicates padding.
+#         Parameters:
+#         q (torch.Tensor): The query for attention, with shape (batch_size, hidden_size).
+#         k (torch.Tensor): The keys for attention, with shape (batch_size, seq_len, hidden_size).
+#         v (torch.Tensor): The values for attention, with shape (batch_size, seq_len, hidden_size).
+#         mask (torch.Tensor, optional): The attention mask, with shape (batch_size, seq_len). 1 indicates valid token, 0 indicates padding.
 
-        Returns:
-        torch.Tensor: The context vector after attention, with shape (batch_size, hidden_size).
+#         Returns:
+#         torch.Tensor: The context vector after attention, with shape (batch_size, hidden_size).
 
-        Functionality:
-        1. Linearly project the keys, values and query using the module's linear layers.
-        2. Split the projected keys, values and query into multiple heads.
-        3. Apply scaled dot-product attention for each head.
-        4. Concatenate the attention outputs of each head.
-        5. Project the concatenated output through the output linear layer.
-        """
-        N = q.shape[0]
+#         Functionality:
+#         1. Linearly project the keys, values and query using the module's linear layers.
+#         2. Split the projected keys, values and query into multiple heads.
+#         3. Apply scaled dot-product attention for each head.
+#         4. Concatenate the attention outputs of each head.
+#         5. Project the concatenated output through the output linear layer.
+#         """
+#         N = q.shape[0]
 
-        q_len, k_len, v_len = q.shape[1], k.shape[1], v.shape[1]
+#         q_len, k_len, v_len = q.shape[1], k.shape[1], v.shape[1]
 
-        # Apply projection layer.
-        q = self.query(q)
-        k = self.key(k)
-        v = self.value(v)
+#         # Apply projection layer.
+#         q = self.query(q)
+#         k = self.key(k)
+#         v = self.value(v)
 
-        # Reshape for multi-head attention.
-        q = q.reshape(N, q_len, self.num_heads, self.head_size)
-        k = k.reshape(N, k_len, self.num_heads, self.head_size)
-        v = v.reshape(N, v_len, self.num_heads, self.head_size)
+#         # Reshape for multi-head attention.
+#         q = q.reshape(N, q_len, self.num_heads, self.head_size)
+#         k = k.reshape(N, k_len, self.num_heads, self.head_size)
+#         v = v.reshape(N, v_len, self.num_heads, self.head_size)
 
-        # Compute scaled dot-product attention.
-        attn = self.scaled_dot_product_attention(q, k, v)
+#         # Compute scaled dot-product attention.
+#         attn = self.scaled_dot_product_attention(q, k, v)
 
-        # Reshape for multi-head attention.
-        attn = attn.reshape(N, q_len, self.num_heads * self.head_size)
-        # Apply output projection layer.
-        output = self.output(attn)
-        return output
+#         # Reshape for multi-head attention.
+#         attn = attn.reshape(N, q_len, self.num_heads * self.head_size)
+#         # Apply output projection layer.
+#         output = self.output(attn)
+#         return output
 
 
 class FeedForwardModel(nn.Module):
@@ -201,8 +202,6 @@ class FeedForwardModel(nn.Module):
             norm_type = self.config.get("norm_type", "layer_norm")
             if norm_type == "layer_norm":
                 layers.append(nn.LayerNorm(in_size))
-            elif norm_type == "batch_norm":
-                layers.append(nn.BatchNorm1d(in_size))
             elif norm_type == "rms_norm":
                 layers.append(RMSNorm(self.config, in_size))
         layers.append(nn.Linear(in_size, out_size))
@@ -215,8 +214,6 @@ class FeedForwardModel(nn.Module):
             norm_type = self.config.get("norm_type", "layer_norm")
             if norm_type == "layer_norm":
                 layers.append(nn.LayerNorm(out_size))
-            elif norm_type == "batch_norm":
-                layers.append(nn.BatchNorm1d(out_size))
             elif norm_type == "rms_norm":
                 layers.append(RMSNorm(self.config, out_size))
 
@@ -244,6 +241,27 @@ class FeedForwardLM(LanguageModule):
         return x
 
 
+class FeedForward(nn.Module):
+    """a simple feed forward layer followed by a non-linearity"""
+
+    def __init__(self, config: Dict[str, Any]):
+        super().__init__()
+        assert "embed_dim" in config, "FeedForwardModel requires an embed_dim."
+        self.config = config
+        embed_dim = config["embed_dim"]
+        dropout = config.get("dropout_ratio", 0.01)
+        # Feed forward layer is 4x wider of the embedding dimension
+        self.net = nn.Sequential(
+            nn.Linear(embed_dim, 4 * embed_dim),
+            nn.ReLU(),
+            nn.Linear(4 * embed_dim, embed_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
 class TransformerBlock(nn.Module):
     def __init__(self, config: Dict[str, Any]):
         super().__init__()
@@ -257,13 +275,10 @@ class TransformerBlock(nn.Module):
             dropout=self.dropout_ratio,
             batch_first=True,
         )
-        config.update({"ffm_layer_sizes": [self.embed_dim, self.embed_dim]})
-        self.feed_forward = FeedForwardModel(config)
+        self.feed_forward = FeedForward(config)
 
         self.norm1 = nn.LayerNorm(self.embed_dim)
         self.norm2 = nn.LayerNorm(self.embed_dim)
-
-        self.dropout = nn.Dropout(self.dropout_ratio)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for the Transformer block.
@@ -277,11 +292,9 @@ class TransformerBlock(nn.Module):
         # Attention with residual.
         x = self.norm1(x)
         attn_output, _ = self.attention(x, x, x)
-        x = x + self.dropout(attn_output)
+        x = x + attn_output
         # Feed forward with residual.
-        x = self.norm2(x)
-        ff_output = self.feed_forward(x)
-        x = x + self.dropout(ff_output)
+        x = x + self.feed_forward(self.norm2(x))
         return x
 
 
@@ -302,8 +315,8 @@ class StackedTransformer(nn.Module):
         Args:
             x (torch.Tensor): The input tensor. Shape: [batch_size, seq_len, embed_dim].
         """
-        # Pass the input through the stacked transformer blocks
-        x = self.stack(x)
+        for layer in self.stack:
+            x = layer(x)
         return x
 
 
@@ -320,7 +333,8 @@ class SinCosPositionalEmbedding(nn.Module):
         self.register_buffer("pos_emb", pos_emb)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x + self.pos_emb[: x.size(1), :]
+        seq_len = x.size(1)  # pos embedding is only relevant to seq_len
+        return self.pos_emb[:seq_len, :]
 
 
 class TransformerLM(LanguageModule):
@@ -346,32 +360,34 @@ class TransformerLM(LanguageModule):
         output = model(x)
     """
 
-    def __init__(self, config: Dict[str, Any]):
+    def __init__(self, config: Dict[str, Any], device):
         super().__init__(config)
         assert "embed_dim" in config, "embed_dim must be specified in config"
-        embed_dim = config["embed_dim"]
+        self.embed_dim = config["embed_dim"]
         assert "vocab_size" in config, "vocab_size must be specified in config"
         vocab_size = config["vocab_size"]
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.position_embedding = SinCosPositionalEmbedding(
-            seq_len=config["context_size"], embed_dim=config["embed_dim"]
-        )
+        self.use_position_embedding = config.get("use_position_embedding", True)
+
+        # Embedding for tokens
+        self.embedding = nn.Embedding(vocab_size, self.embed_dim)
+
         self.stacked_transformer = StackedTransformer(config)
-        self.projection_layer = nn.Linear(embed_dim, vocab_size)
+        self.layer_norm = nn.LayerNorm(self.embed_dim)
+        self.projection_layer = nn.Linear(self.embed_dim, vocab_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass for the TransformerLM.
+        batch_size, seq_len = x.shape
+        # Token embedding
+        x = self.embedding(x)  # [batch_size, seq_len, embed_dim]
+        assert x.shape == (
+            batch_size,
+            seq_len,
+            self.embed_dim,
+        ), f"x.shape = {x.shape}, expected shape ({batch_size}, {seq_len}, {self.embed_dim})"
 
-        Args:
-            x (torch.Tensor): The input tensor containing token IDs.
-                Shape: [batch_size, seq_len].
-
-        Returns:
-            torch.Tensor: The output tensor after passing through the embedding and transformer layers.
-                Shape: [batch_size, seq_len, vocab_size].
-        """
-        x = self.embedding(x)
-        x = self.position_embedding(x)
+        # Passing through stacked transformers with attention mask
         x = self.stacked_transformer(x)
+        x = self.layer_norm(x)
         logits = self.projection_layer(x)
+
         return logits
