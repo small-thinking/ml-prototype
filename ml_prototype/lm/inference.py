e92e02ab63372450a184a1236cc90bf5719bc1b1
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-09-30 15:42:22 -0700

    Add Byte-pair tokenizer

--- ml_prototype/lm/inference.py
+++ ml_prototype/lm/inference.py
@@ -2,19 +2,23 @@ import argparse
 import torch
 import os
 from torch.nn.functional import softmax
-from tokenizer import NaiveTokenizer, Tokenizer
+from tokenizer import NaiveTokenizer, Tokenizer, BytePairTokenizer
 
 
 class InferenceEngine:
-    def __init__(self, tokenizer, jit_model_path: str, device: str):
+    def __init__(self, tokenizer_type: str, token_folder_path: str, jit_model_path: str, device: str):
         self.device = device
         self.model = torch.jit.load(jit_model_path).to(self.device)
         self.model.eval()
-        self.tokenizer = tokenizer
+        self.tokenizer_type = tokenizer_type
+        if self.tokenizer_type == 'bpe':
+            self.tokenizer = BytePairTokenizer(config={"token_folder_path": token_folder_path})
+        else:
+            self.tokenizer = NaiveTokenizer(config={"token_folder_path": token_folder_path})
 
     def inference(self, text: str, max_length: int = 256, temperature: float = 1.0):
-        print(f"Inference: {text}")
-        input_tensor = self.tokenizer.encode([text]).to(self.device)
+        print(f"Inference: {text}\n")
+        input_tensor = self.tokenizer.encode(text).unsqueeze(0).to(self.device)
         generated_sequence = input_tensor
 
         with torch.no_grad():
@@ -28,13 +32,11 @@ class InferenceEngine:
                 # Apply softmax to get probabilities
                 adj_prob = softmax(adj_logits, dim=-1)
 
-                next_token = torch.multinomial(adj_prob, num_samples=1).to(
-                    self.device
-                )  # Move tensor to CPU
+                next_token = torch.multinomial(adj_prob, num_samples=1).to(self.device)
                 generated_sequence = torch.cat([generated_sequence, next_token], dim=1)
 
                 # Decode the next token to text and yield it
-                next_token_text = self.tokenizer.decode(next_token[0])
+                next_token_text = self.tokenizer.decode(next_token[0])[0]
                 yield next_token_text
 
                 if generated_sequence.size(1) >= max_length:
@@ -53,17 +55,15 @@ def main():
     parser.add_argument('--max_length', "-m", type=int, default=256, help='Maximum length of generated sequence.')
     parser.add_argument('--temperature', "-t", type=float, default=1.0, help='Temperature for sampling.')
     parser.add_argument('--device', "-d", type=str, default="cuda:0", help='The device to use.')
+    parser.add_argument('--tokenizer_type', "-tt", type=str, default="char", help='Tokenizer type: "char" or "bpe".')
+    parser.add_argument('--token_folder_path', "-tf", type=str, default="./tokenizer/char", help='The folder path of the token.')
 
     # Parse the arguments
     args = parser.parse_args()
-
-    token_file = os.path.expanduser("./data/tokens.json")
-    tokenizer = NaiveTokenizer(config={}, token_file=token_file)
     inference_engine = InferenceEngine(
-        tokenizer=tokenizer,
-        jit_model_path=os.path.join(
-            os.path.dirname(__file__), "../../model_epoch_30.pt"
-        ),
+        tokenizer_type=args.tokenizer_type,
+        token_folder_path=args.token_folder_path,
+        jit_model_path=os.path.join(os.path.dirname(__file__), "../../model_epoch_10.pt"),
         device=args.device,
     )
 
